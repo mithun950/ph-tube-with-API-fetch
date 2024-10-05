4f07f9ebfc267bade04c17bc5595205595ef8e81
mithun950 <mithunrony46@gmail.com>
2024-10-05 16:47:32 +0600

    first commit

--- scripts/video.js
+++ scripts/video.js
@@ -6,6 +6,12 @@ function getTimeString(time){
     remainingSecond = remainingSecond % 60;
     return `${hour} hour ${minutes} minutes ${remainingSecond} seconds ago`;
 }
+const removeActiveBtn = () => {
+ const buttons = document.getElementsByClassName('category-btn');
+ for(let btn of buttons ){
+    btn.classList.remove('active');
+ }
+};
 
 //1- fetch load and categories on html
 //create load categories
@@ -20,10 +26,10 @@ const loadCategories = async() =>{
    };
 
 //    create load videos 
-const loadVideos = async() =>{
+const loadVideos =(searchText='') =>{
     //fetch the data
     
-     fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+     fetch(`https://openapi.programming-hero.com/api/phero-tube/videos?title= ${searchText}`)
     .then(res => res.json())
     .then(data => displayVideos(data.videos))
     .catch((error) => console.log(error));
@@ -33,12 +39,30 @@ const loadVideos = async() =>{
     const loadCategoryVideos = (id) => {
         fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
         .then(res => res.json())
-        .then(data => displayVideos(data.category))
+        .then(data => {
+          const activeButton = document.getElementById(`btn-${id}`);
+          removeActiveBtn()
+          displayVideos(data.category)
+          activeButton.classList.add('active')
+        })
         .catch((error) => console.log(error));
      
 
     }
-
+const loadDetails = async(videoId) =>{
+    const url =`https://openapi.programming-hero.com/api/phero-tube/video/${videoId}`
+    const res = await fetch(url)
+    const data = await res.json();
+    displayDetails(data.video);
+}
+const displayDetails = (video) =>{
+   const detailsContainer = document.getElementById('modal-content');
+   document.getElementById('customModal').showModal();
+   detailsContainer.innerHTML = `
+   <img src=${video.thumbnail}/>
+   <p>${video.description}</p>
+   `
+}
     // const demoObject =
     // {
     //     "category_id": "1001",
@@ -109,11 +133,9 @@ const loadVideos = async() =>{
      <p class= 'text-gray-400'> ${video.authors[0].profile_name} </p>
      ${video.authors[0].verified === true ? `<img class='h-3 w-3' src='https://img.icons8.com/?size=96&id=D9RtvkuOe31p&format=png'/>` : " "}
     
-    <p>
     
-    </p>
     </div>
-   
+      <p><button onclick="loadDetails('${video.video_id}')" class="btn btn-sm btn-error ">details</button></p>
     </div>
     </div>
   </div>
@@ -134,7 +156,7 @@ const displayCategories = (categories) =>{
 // //  create a button 
  const buttonContainer = document.createElement('div');
  buttonContainer.innerHTML=`
- <button onclick="loadCategoryVideos(${item.category_id})" class="btn">
+ <button id="btn-${item.category_id}" onclick="loadCategoryVideos(${item.category_id})" class="btn category-btn">
  ${item.category}
  </button>
  
@@ -145,6 +167,8 @@ categoryContainer.append(buttonContainer)
     });
 
 };
-
+document.getElementById('search-input').addEventListener('keyup',(e)=>{
+    loadVideos(e.target.value)
+})
 loadCategories();
 loadVideos();
